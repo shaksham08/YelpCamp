0c59c04b260d8dd367ccc718d5cbb0e4fd10b778
shaksham08 <shakshamsinha@outlook.com>
2020-04-22 13:59:40 +0530

    DOne with some changes

--- app.js
+++ app.js
@@ -1,55 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const port = 8000;
 
-var campgrounds = [
-  {
-    name: "Salmon Greek",
-    image:
-      "https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "mount california",
-    image:
-      "https://images.unsplash.com/photo-1528433556524-74e7e3bfa599?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "Norway",
-    image:
-      "https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "Salmon Greek",
-    image:
-      "https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "mount california",
-    image:
-      "https://images.unsplash.com/photo-1528433556524-74e7e3bfa599?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "Norway",
-    image:
-      "https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "Salmon Greek",
-    image:
-      "https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "mount california",
-    image:
-      "https://images.unsplash.com/photo-1528433556524-74e7e3bfa599?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "Norway",
-    image:
-      "https://images.unsplash.com/photo-1513104399965-f5160d963d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-];
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
+
+const Campground = mongoose.model("campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -58,25 +25,56 @@ app.get("/", (req, res) => {
   res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  //get all campgrounds from db
+  Campground.find({}, (err, allcampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allcampgrounds });
+    }
+  });
+  //
 });
 
+//NEw : show fporm to create campground
 //* getting data from the form
 app.post("/campgrounds", (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
+  var description = req.body.description;
   var newCampground = {
     name: name,
     image: image,
+    description: description,
   };
-  campgrounds.push(newCampground);
 
-  res.redirect("./campgrounds");
+  // create a new campground and save to the databse
+  Campground.create(newCampground, (err, newAddedCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(newAddedCampground);
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", function (req, res) {
   res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+  //find the campground with provided id and show the details
+
+  Campground.findById(req.params.id, (err, foundCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampgrounds });
+    }
+  });
 });
 
 app.listen(port, () => {
